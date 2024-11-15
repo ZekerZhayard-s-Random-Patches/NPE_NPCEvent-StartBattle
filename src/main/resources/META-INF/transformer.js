067a31bcb0afd7315e54055579aa5e32f0abb4c0
ZekerZhayard <trees1000@qq.com>
2024-11-15 12:17:29 +0800

    1.20.2-1.1

--- src/main/resources/META-INF/transformer.js
+++ src/main/resources/META-INF/transformer.js
@@ -21,6 +21,24 @@ function initializeCoreMod() {
                 }
                 return mn;
             }
+        },
+        "NPCEvent$EndBattle_<init>": {
+            "target": {
+                "type": "METHOD",
+                "class": "com/pixelmonmod/pixelmon/api/events/npc/NPCEvent$EndBattle",
+                "methodName": "<init>",
+                "methodDesc": "(Lcom/pixelmonmod/pixelmon/battles/controller/BattleController;Lcom/pixelmonmod/pixelmon/api/battles/BattleEndCause;ZLjava/util/Map;)V"
+            },
+            "transformer": function (mn) {
+                var insnList = mn.instructions.toArray();
+                for (var i = 0; i < insnList.length; i++) {
+                    var node = insnList[i];
+                    if (node.getOpcode() === Opcodes.INVOKEVIRTUAL && node.owner.equals("com/pixelmonmod/pixelmon/battles/controller/participants/EntityParticipant") && node.name.equals(ASMAPI.mapField("getEntity")) && node.desc.equals("()Lnet/minecraft/world/entity/Entity;")) {
+                        mn.instructions.set(node, new MethodInsnNode(Opcodes.INVOKESTATIC, "io/github/zekerzhayard/npe_npcevent_startbattle/Hook", "getEntity", "(Lcom/pixelmonmod/pixelmon/battles/controller/participants/EntityParticipant;)Lnet/minecraft/world/entity/Entity;", false));
+                    }
+                }
+                return mn;
+            }
         }
     }
 }
